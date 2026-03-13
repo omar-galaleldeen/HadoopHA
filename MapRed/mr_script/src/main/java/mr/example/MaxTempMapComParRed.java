bed94e8487ac7664ac927787878c77d379a92964
Omar Mostafa <omar.galaleldeen.com>
2026-03-13 03:05:27 +0200

    Added Map Reduce Files

---
+++ MapRed/mr_script/src/main/java/mr/example/MaxTempMapComParRed.java
@@ -0,0 +1,93 @@
+package mr.example;
+
+import java.io.IOException;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Partitioner;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+
+public class MaxTempMapComParRed {
+	public static void main(String[] args) throws Exception {
+		if (args.length != 2) {
+			System.err.println("Usage: MaxTemperatureMapperCombinerPartitionerReducer <input path> <output path>");
+			System.exit(-1);
+		}
+		
+		// Job configuration
+		//Configuration conf = new Configuration();
+		//conf.set("mapreduce.job.queuename", "root.batch_jobs");
+		//Job job = new Job(conf);
+		Job job = new Job();
+		
+		job.setJarByClass(MaxTempMapComParRed.class);
+		job.setJobName("Max temperature");
+		job.setNumReduceTasks(12);
+		// edit the previous line for number of reducers
+		job.setMapperClass(TemperatureMapper1.class);
+		job.setCombinerClass(TemperatureReducer1.class);
+		job.setPartitionerClass(TemperaturePartitioner1.class);
+		job.setReducerClass(TemperatureReducer1.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(DoubleWritable.class);
+	
+
+		// Input & Output
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		// Run Job
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+}
+
+class TemperatureMapper1 extends Mapper<LongWritable, Text, Text, DoubleWritable> {
+	@Override
+	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+		// get value
+		String line = value.toString();
+
+		// Ignore headers
+		if (line.startsWith("STATION")) { return; }
+		
+		// Parse fields
+		String[] fields = line.split("\\|");
+		String year = fields[2].substring(0, 7);
+		double temperature = Double.parseDouble(fields[6]);
+		int qualityCode = Integer.parseInt(fields[8]);
+
+		// output record if valid quality code
+		if (qualityCode == 0 || qualityCode == 1) {
+			context.write(new Text(year), new DoubleWritable(temperature));
+		}
+	}
+}
+
+class TemperaturePartitioner1<K2, V2> extends Partitioner<Text, DoubleWritable> {
+
+	public int getPartition(Text key, DoubleWritable value, int numReduceTasks) {
+		//return (int) (Integer.parseInt(key.toString()) - 1901);
+		String[] parts = key.toString().split("-"); 
+        int month = Integer.parseInt(parts[1]); 
+        return (month - 1) % numReduceTasks; 
+    }
+}
+
+class TemperatureReducer1 extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
+	@Override
+	public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
+		double maxValue = Double.MIN_VALUE;
+		for (DoubleWritable value : values) {
+			maxValue = Math.max(maxValue, value.get());
+		}
+		context.write(key, new DoubleWritable(maxValue));
+	}
+}
